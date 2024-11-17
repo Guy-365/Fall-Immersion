959ebad6cf7377d10b68a368ceb9d9a22586c30c
Guy-365 <amarnie.edwards@hgs.hiddengeniusproject.org>
2024-11-17 15:02:07 -0500

    Update and rename view-2.html to script.js

---
+++ CS/javascript/Calculator/script.js
@@ -0,0 +1,32 @@
+/* 
+    Javascript Checklist
+----------------------------
+1. Constant variable that stores your calculator display
+2. 3 Functions:
+    2a. Function that adds any numbers and operators to the display based off user input
+    2b. Function that changes a number from positive to negative and negative to positive
+    2c. Function to clear the display
+    2d. Function that will correctly calculate the equation
+        - try and catch statement to catch an error
+            * try statement will evaluate the equation and solve it
+            * catch statement will change the display to say "Error"
+            
+
+    **HELP/HINTS AT YOUR DISPOSAL**
+----------------------------------------
+    -----References, Methods, and Attributes to use-----
+    |--------------------------------------------------|
+    | .getElementById()     .value      try...catch    |
+    |                                                  |
+    |    .eval()                                       |
+    |--------------------------------------------------|
+*/
+
+function calculate(){
+    try{
+        //
+    }
+    catch{
+
+    }
+}
