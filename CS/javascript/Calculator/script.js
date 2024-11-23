613d18b7b910fd8ff93dedd816051c6c2c5f8cbb
Guy-365 <amarnie.edwards@hgs.hiddengeniusproject.org>
2024-11-22 22:04:40 -0500

    Update script.js

--- CS/javascript/Calculator/script.js
+++ CS/javascript/Calculator/script.js
@@ -1,41 +1,24 @@
-/* 
-    Javascript Checklist
-----------------------------
-1. Constant variable that stores your calculator display
-2. 3 Functions:
-    2a. Function that adds any numbers and operators to the display based off user input
-    2b. Function that changes a number from positive to negative and negative to positive
-    2c. Function to clear the display
-    2d. Function that will correctly calculate the equation
-        - try and catch statement to catch an error
-            * try statement will evaluate the equation and solve it
-            * catch statement will change the display to say "Error"
-            
-
-    **HELP/HINTS AT YOUR DISPOSAL**
-----------------------------------------
-    -----References, Methods, and Attributes to use-----
-    |--------------------------------------------------|
-    | .getElementById()     .value      try...catch    |
-    |                                                  |
-    |    .eval()                                       |
-    |--------------------------------------------------|
-*/
- const screen = document.getElementById('display');
+const screen = document.getElementById('display');
     
 
-function appendNumber(){
-    
+function AddTogether(input){
+    screen.value += input; 
 }
 
-    
-function appendOperator(){
+function CulcuOperator( ){
     try{ 
-        //
+        screen.value = eval(screen.value);
     }
     catch{
-
+        screen.value = "Error";
     }
 }
 
+function PosNeg(input){
 
+    screen.value = parseFloat(screen.value) * -1;
+}
+
+function clearDisplay(){
+    screen.value = ' '
+}
